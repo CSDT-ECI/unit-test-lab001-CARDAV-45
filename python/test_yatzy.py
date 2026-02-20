3ca614c9ee6b9b03a85e8625f3589892e2d2f005
CARDAV-45 <barrerovelasquezcarlosdavid@gmail.com>
2026-02-20 09:41:55 -0500

    add unit tests with MethodName_StateUnderTest_ExpectedBehavior pattern

--- python/test_yatzy.py
+++ python/test_yatzy.py
@@ -3,8 +3,20 @@ from yatzy import Yatzy
 # These unit tests can be run using the py.test framework
 # available from http://pytest.org/
 
+def test_chance_mixedDice_returnsCorrectSum():
+    result = Yatzy.chance(1, 2, 3, 4, 5)
+    assert result == 15
 
-def test_demo():
-        expected = 15
-        actual = 15
-        assert expected == actual        
+def test_yatzy_allDiceSame_returns50():
+    result = Yatzy.yatzy([1, 1, 1, 1, 1])
+    assert result == 50
+
+
+def test_ones_multipleOnes_returnsSumOfOnes():
+    result = Yatzy.ones(1, 1, 1, 2, 3)
+    assert result == 3
+
+
+def test_twos_multipleTwos_returnsSumOfTwos():
+    result = Yatzy.twos(2, 2, 3, 4, 5)
+    assert result == 4
